273d2f767d21ab6cafefa8f9056b036c6c1694d8
bdreis <felipebadodosreis@gmail.com>
2025-05-02 23:38:26 +0200

    Adiciona estrutura básica do TGV Max Explorer

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,18 @@ module.exports = function(eleventyConfig) {
   // Copiar arquivos estáticos
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
-  
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  // Configurar diretórios
   return {
     dir: {
       input: "src",
       output: "_site",
-      includes: "_includes"
-    }
+      includes: "_includes",
+      layouts: "_includes/layouts"
+    },
+    templateFormats: ["html", "md", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
   };
 };
